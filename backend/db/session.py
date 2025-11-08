fc2f41a46239a2c941b5a3315a28f00cc591e768
Josue Venegas <josuevenegasalmonacid@gmail.com>
2025-11-08 19:54:47 -0300

    Update session management to use custom Session class in sessionmaker to fix session object no exec attribute error

--- backend/db/session.py
+++ backend/db/session.py
@@ -7,7 +7,7 @@ from core.config import settings
 
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URI) # type: ignore
 
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+SessionLocal = sessionmaker(bind=engine, class_=Session, autocommit=False, autoflush=False)
 
 
 def get_db() -> Generator[Session, None, None]:
